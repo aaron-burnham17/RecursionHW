04dbf0eb62f4bd830644b1c94fbe31727db6703b
aaron-burnham17 <argust34@gmail.com>
2018-04-22 11:23:57 -0700

    Tests working using junit5

---
+++ src/test/java/edu.gcccd.csis/RecursionTest.java
@@ -0,0 +1,28 @@
+package edu.gcccd.csis;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecursionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void printEveryOther(){
+
+    }
+
+    @Test
+    public void printStars(){
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
