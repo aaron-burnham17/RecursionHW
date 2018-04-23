72fec251dfd81937c4575debecdebb293328c048
aaron-burnham17 <argust34@gmail.com>
2018-04-23 14:36:44 -0700

    sum test added

--- src/test/java/edu.gcccd.csis/RecursionTest.java
+++ src/test/java/edu.gcccd.csis/RecursionTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static edu.gcccd.csis.Recursion.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,5 +36,25 @@ class RecursionTest {
         assertEquals("",printStars(""));
         assertEquals(null,printStars(null));
 
+    }
+
+    /*
+     * we know the sum of the 1st k positive numbers is n(n+1)2
+     */
+    @Test
+    public void sum() {
+        for (int i = 0; i < 10; i++) {
+            int k = new Random().nextInt(100);
+            assertEquals(k * (k + 1) / 2, Recursion.sum(k));
+        }
+        // corner cases
+        assertEquals(0, Recursion.sum(-1));
+        assertEquals(0, Recursion.sum(0));
+        try {
+            int x = Recursion.sum(Integer.MAX_VALUE); // stackoverflow
+            fail("");
+        } catch (StackOverflowError error) {
+            assertTrue(true);
+        }
     }
 }
